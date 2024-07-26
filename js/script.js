c3d4f8b8ca3880ec28789075fc31bda211d92a09
GM1339 <121707372+GM1339@users.noreply.github.com>
2024-07-26 11:20:08 -0700

    Update script.js revert to SAFE

--- js/script.js
+++ js/script.js
@@ -87,29 +87,20 @@ function move(direction)
 
     switch (direction) 
     {
-// Assuming `portalFacing` is already defined and set to one of 'north', 'south', 'east', or 'west'.
-
-switch (direction) 
-{
-switch (direction) 
-{
-    case 'north':
-        if (playerPosition.y > 0) playerPosition.y--;
-        break;
-    case 'south':
-        if (playerPosition.y < vaultSize - 1) playerPosition.y++;
-        break;
-    case 'east':
-        if (playerPosition.x < vaultSize - 1) playerPosition.x++;
-        break;
-    case 'west':
-        if (playerPosition.x > 0) playerPosition.x--;
-        break;
-}
-
-}
-
+        case 'north':
+            if (playerPosition.y > 0 && !(playerPosition.x === currentRoom.x && playerPosition.y - 1 === currentRoom.y)) playerPosition.y--;
+            break;
+        case 'south':
+            if (playerPosition.y < vaultSize - 1 && !(playerPosition.x === currentRoom.x && playerPosition.y + 1 === currentRoom.y)) playerPosition.y++;
+            break;
+        case 'east':
+            if (playerPosition.x < vaultSize - 1 && !(playerPosition.x + 1 === currentRoom.x && playerPosition.y === currentRoom.y)) playerPosition.x++;
+            break;
+        case 'west':
+            if (playerPosition.x > 0 && !(playerPosition.x - 1 === currentRoom.x && playerPosition.y === currentRoom.y)) playerPosition.x--;
+            break;
     }
+
     if (!roomData[`${playerPosition.x},${playerPosition.y}`]?.discovered) {
         roomData[`${playerPosition.x},${playerPosition.y}`] = { type: 'normal', discovered: true, completed: false };
     }
